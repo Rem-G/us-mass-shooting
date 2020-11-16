60f8588214a39d3ab30b4d0853952655c71174a6
Rem-G <gosselin.rem@gmail.com>
2020-11-16 15:35:05 +0100

    Server update, add d3

--- api/views.py
+++ api/views.py
@@ -1,7 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from tools import Tools
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
+
 tools = Tools()
 
 @app.route('/')
